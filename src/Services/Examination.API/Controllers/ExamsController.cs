0a4aff9befcfd08f9c040c3c5251085a7f316c28
huytq <huytq3103@gmail.com>
2024-10-27 09:26:51 +0700

    Add API Exam

---
+++ src/Services/Examination.API/Controllers/ExamsController.cs
@@ -0,0 +1,96 @@
+﻿using Examination.Application.Commands.Exams.CreateExam;
+using Examination.Application.Commands.Exams.DeleteExam;
+using Examination.Application.Commands.Exams.UpdateExam;
+using Examination.Application.Queries.Exams.GetAllExams;
+using Examination.Application.Queries.Exams.GetExamById;
+using Examination.Application.Queries.Exams.GetExamsPaging;
+using Examination.Shared.Exams;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examination.API.Controllers
+{
+    public class ExamsController : BaseController
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ExamsController> _logger;
+
+        public ExamsController(IMediator mediator, ILogger<ExamsController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllExams()
+        {
+            var result = await _mediator.Send(new GetAllExamsQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("paging")]
+        public async Task<IActionResult> GetAllExamsPaging([FromQuery] GetExamsPagingQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetExamById(string id)
+        {
+            var result = await _mediator.Send(new GetExamByIdQuery(id));
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateExamAsync([FromBody] CreateExamRequest request)
+        {
+            var command = new CreateExamCommand()
+            {
+                Name = request.Name,
+                AutoGenerateQuestion = request.AutoGenerateQuestion,
+                CategoryId = request.CategoryId,
+                Content = request.Content,
+                Duration = request.Duration,
+                IsTimeRestricted = request.IsTimeRestricted,
+                Level = request.Level,
+                NumberOfQuestionCorrectForPass = request.NumberOfQuestionCorrectForPass,
+                NumberOfQuestions = request.NumberOfQuestions,
+                Questions = request.Questions,
+                ShortDesc = request.ShortDesc,
+            };
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateExamAsync([FromBody] UpdateExamRequest request)
+        {
+            var command = new UpdateExamCommand()
+            {
+                Id = request.Id,
+                Name = request.Name,
+                AutoGenerateQuestion = request.AutoGenerateQuestion,
+                CategoryId = request.CategoryId,
+                Content = request.Content,
+                Duration = request.Duration,
+                IsTimeRestricted = request.IsTimeRestricted,
+                Level = request.Level,
+                NumberOfQuestionCorrectForPass = request.NumberOfQuestionCorrectForPass,
+                NumberOfQuestions = request.NumberOfQuestions,
+                Questions = request.Questions,
+                ShortDesc = request.ShortDesc,
+            };
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExamAsync(string id)
+        {
+            var result = await _mediator.Send(new DeleteExamCommand(id));
+            return Ok(result);
+        }
+    }
+}
--- src/Services/Examination.Application/Queries/Exams/GetExamsPaging/GetExamsPagingQuery.cs
+++ src/Services/Examination.Application/Queries/Exams/GetExamsPaging/GetExamsPagingQuery.cs
@@ -6,8 +6,8 @@ namespace Examination.Application.Queries.Exams.GetExamsPaging
 {
     public class GetExamsPagingQuery : IRequest<ApiResult<PagedList<ExamDto>>>
     {
-        public string CategoryId { get; set; }
-        public string SearchKeyword { get; set; }
+        public string? CategoryId { get; set; }
+        public string? SearchKeyword { get; set; }
         public int PageIndex { get; set; }
         public int PageSize { get; set; }
     }
