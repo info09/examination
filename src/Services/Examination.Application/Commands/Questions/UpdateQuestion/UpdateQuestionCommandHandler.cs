dc1e4ccf4bd692aaf38a2274752f4453e2f1ed29
huytq@ics-p.vn <huytq@ics-p.vn>
2024-10-31 13:40:45 +0700

    Update Question and refactor code

--- src/Services/Examination.Application/Commands/Questions/UpdateQuestion/UpdateQuestionCommandHandler.cs
+++ src/Services/Examination.Application/Commands/Questions/UpdateQuestion/UpdateQuestionCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Examination.Domain.AggregateModels.CategoryAggregate;
 using Examination.Domain.AggregateModels.QuestionAggregate;
 using Examination.Shared.Questions;
 using Examination.Shared.SeedWorks;
@@ -10,14 +11,17 @@ namespace Examination.Application.Commands.Questions.UpdateQuestion
     public class UpdateQuestionCommandHandler : IRequestHandler<UpdateQuestionCommand, ApiResult<bool>>
     {
         private readonly IQuestionRepository _questionRepository;
+        private readonly ICategoryRepository _categoryRepository;
         private readonly ILogger<UpdateQuestionCommandHandler> _logger;
         private readonly IMapper _mapper;
 
         public UpdateQuestionCommandHandler(
                 IQuestionRepository QuestionRepository,
+                ICategoryRepository CategoryRepository,
                 ILogger<UpdateQuestionCommandHandler> logger, IMapper mapper
             )
         {
+            _categoryRepository = CategoryRepository;
             _questionRepository = QuestionRepository;
             _logger = logger;
             _mapper = mapper;
@@ -27,6 +31,7 @@ namespace Examination.Application.Commands.Questions.UpdateQuestion
         public async Task<ApiResult<bool>> Handle(UpdateQuestionCommand request, CancellationToken cancellationToken)
         {
             var itemToUpdate = await _questionRepository.GetQuestionsByIdAsync(request.Id);
+            var category = await _categoryRepository.GetCategoriesByIdAsync(request.CategoryId);
             if (itemToUpdate == null)
             {
                 _logger.LogError($"Item is not found {request.Id}");
@@ -37,6 +42,7 @@ namespace Examination.Application.Commands.Questions.UpdateQuestion
             itemToUpdate.QuestionType = request.QuestionType;
             itemToUpdate.Level = request.Level;
             itemToUpdate.CategoryId = request.CategoryId;
+            itemToUpdate.CategoryName = category.Name;
             var answers = _mapper.Map<List<AnswerDto>, List<Answer>>(request.Answers);
             itemToUpdate.Answers = answers;
 
